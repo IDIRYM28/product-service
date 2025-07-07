1ff9319bb069aaec18e78135dcc44c990cc5d60c
IDIRYM28 <idiryahiamessaoud@gmail.com>
2025-07-07 21:09:38 +0200

    first commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
